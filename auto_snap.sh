00059c111de850c6a16d8fb0a22f4707d802aba6
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2023-03-06 14:33:26 +0300

    Update auto_snap.sh

--- auto_snap.sh
+++ auto_snap.sh
@@ -2,12 +2,12 @@
 
 while true
 do
-sudo systemctl stop nolusd
+systemctl stop nolusd
 cp $HOME/.nolus/data/priv_validator_state.json $HOME/.nolus/priv_validator_state.json.backup
 rm -rf $HOME/.nolus/data
 curl -L https://snapshots.kjnodes.com/nolus-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.nolus
 mv $HOME/.nolus/priv_validator_state.json.backup $HOME/.nolus/data/priv_validator_state.json
-sudo systemctl start nolusd
+systemctl start nolusd
 sleep 300
 bash /root/nolus_snap/nolus_snap.sh
 sleep 43200
